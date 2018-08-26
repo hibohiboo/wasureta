c826b63b3a90cfd7c81b3183f94d6ba193185184
hibohiboo <swdice66@gmail.com>
2018-08-26 18:19:51 +0900

    開発準備。pug, typescript

--- docker/config/webpack/webpack.config.js
+++ docker/config/webpack/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const globule = require('globule')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // Dependencies
 // yarn add apply-loader autoprefixer babel-core babel-loader babel-preset-env css-loader copy-webpack-plugin extract-text-webpack-plugin@next globule node-sass postcss-loader pug pug-loader raw-loader sass-loader webpack webpack-cli webpack-serve
@@ -12,7 +13,7 @@ const opts = {
 }
 
 const extensionConversions = {
-  pug: 'html',
+  // pug: 'html',
   sass: 'css',
   js: 'js',
   ts: 'js'
@@ -21,15 +22,17 @@ const extensionConversions = {
 const files = {}
 Object.keys(extensionConversions).forEach(from => {
   const to = extensionConversions[from]
-  globule.find([`**/*.${from}`, `!**/_*.${from}`], {cwd: opts.src}).forEach(filename => {
+  globule.find([`assets/**/*.${from}`, `!assets/**/_*.${from}`], {cwd: opts.src}).forEach(filename => {
     files[filename.replace(new RegExp(`.${from}$`, 'i'), `.${to}`)] = path.join(opts.src, filename)
   })
 })
 
-const pugLoader = [
-  'apply-loader',
-  'pug-loader'
-]
+const pugLoader = {
+  use:[
+    'apply-loader',
+    'pug-loader'
+  ]
+};
 
 const sassLoader = [
   {
@@ -65,10 +68,16 @@ module.exports = {
   mode: process.env.ENV ? 'development' : 'production',
   module: {
     rules: [
-      {
-        test: /\.pug$/,
-        use: ExtractTextPlugin.extract(pugLoader)
-      },
+      // {
+      //   test: /\.pug$/,
+      //   use: ExtractTextPlugin.extract(pugLoader),
+      //   // use: [{
+      //   //   loader: 'apply-loader'
+      //   // }, {
+      //   //   loader: 'pug-loader',
+      //   //   options: { pretty: true }
+      //   // }]
+      // },
       {
         test: /\.ts$/,
         use: 'ts-loader'
@@ -109,7 +118,7 @@ module.exports = {
   plugins: [
     new ExtractTextPlugin('[name]'),
     new CopyWebpackPlugin(
-      [{from: {glob: '**/*', dot: true}}],
+      [{from: {glob: 'assets/**/*', dot: true}}],
       {ignore: Object.keys(extensionConversions).map((ext) => `*.${ext}`)}
     )
   ],
@@ -124,9 +133,9 @@ module.exports = {
   // 開発サーバの設定
   devServer: {
     // // public/index.htmlをデフォルトのホームとする
-    // contentBase: './public',
+    contentBase: './dist',
     // // バンドルしたファイルを/assets/js/フォルダに出力したものとする。
-    // publicPath: "/assets/js/",
+    // publicPath: "/assets/",
     // インラインモード
     inline: true,
     // 8080番ポートで起動
