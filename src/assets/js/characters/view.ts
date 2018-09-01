f7b4ed404b0a9a04435356f19c5cc8e7dd8ace55
hibohiboo <swdice66@gmail.com>
2018-09-01 23:23:05 +0900

    パラメータ追加

--- src/assets/js/characters/view.ts
+++ src/assets/js/characters/view.ts
@@ -3,7 +3,20 @@ const mountNode = document.getElementById('main');
 const char = {
     name: "ごん", 
     image: {
-      src: "/assets/images/gon.jpg"
+      src: "/assets/images/gon.jpg",
+      filename: "ごんぎつね",
+      maker: "kyasaba",
+      makerUrl: "https://www44.atwiki.jp/kyasaba/"
+    },
+    profile: "記憶喪失。覚えているのはごんという名前と、好物はおあげさんということ。",
+    parameters: {
+      str: 4,
+      dex: 6,
+      sense: 7,
+      mind: 3,
+      luck: 5,
+      free: 10,
+      freeName: "野生"
     }
   };
 const app = Elm.Main.embed(mountNode, JSON.stringify(char));
--- src/assets/js/elm/characters/views/Main.elm
+++ src/assets/js/elm/characters/views/Main.elm
@@ -4,10 +4,21 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Json.Decode as Decode exposing (Value, Decoder, decodeString, field, string)
 import Json.Decode.Pipeline exposing (decode, required, optional)
-import Html.Events exposing (..)
+-- import Html.Events exposing (..)
 
 
 port logout : () -> Cmd msg
+
+type alias CharacterParameters = 
+  {
+      str: Int,
+      dex: Int,
+      sense: Int,
+      mind: Int,
+      luck: Int,
+      free: Int,
+      freeName: String
+  }
 
 type alias CharacterImage =
     { src: String,
@@ -15,10 +26,21 @@ type alias CharacterImage =
       maker: String,
       makerUrl: String
     }
+
 type alias Character =
     { name : String,
+      profile: String,
+      parameters: CharacterParameters,
       image: Maybe CharacterImage
     }
+
+decoder : Decoder Character
+decoder =
+    decode Character
+        |> Json.Decode.Pipeline.required "name" Decode.string
+        |> Json.Decode.Pipeline.required "profile" Decode.string
+        |> Json.Decode.Pipeline.required "parameters" parametersDecoder
+        |> Json.Decode.Pipeline.optional "image" (Decode.nullable imageDecoder) Nothing
 
 imageDecoder : Decoder CharacterImage
 imageDecoder =
@@ -28,12 +50,17 @@ imageDecoder =
         |> Json.Decode.Pipeline.optional "maker" Decode.string ""
         |> Json.Decode.Pipeline.optional "makerUrl" Decode.string ""
 
-decoder : Decoder Character
-decoder =
-    decode Character
-        |> Json.Decode.Pipeline.required "name" Decode.string
-        |> Json.Decode.Pipeline.optional "image" (Decode.nullable imageDecoder) Nothing
-
+parametersDecoder : Decoder CharacterParameters
+parametersDecoder =
+    decode CharacterParameters
+        |> Json.Decode.Pipeline.required "str" Decode.int
+        |> Json.Decode.Pipeline.required "dex" Decode.int
+        |> Json.Decode.Pipeline.required "sense" Decode.int
+        |> Json.Decode.Pipeline.required "mind" Decode.int
+        |> Json.Decode.Pipeline.required "luck" Decode.int
+        |> Json.Decode.Pipeline.required "free" Decode.int
+        |> Json.Decode.Pipeline.required "freeName" Decode.string
+        
 
 decodeCharacterFromJson : Value -> Maybe Character
 decodeCharacterFromJson json =
@@ -106,12 +133,21 @@ view model =
                     ]
                   ]
                ]
+
 charParameter : Character -> Html Msg
 charParameter char = 
+        div[][
           div[class "row"][
-                       div[class "col l3"][text "名前"],
-                       div[class "col l3"][text char.name]
+              div[class "col l4"][text "名前"],
+              div[class "col l8"][text char.name]
+          ],
+          charParameters char.parameters,
+          div[class "row"][
+              div[class "col l4"][text "プロフィール"],
+              div[class "col l8"][text char.profile]
           ]
+        ]
+
 
 charImage : Maybe CharacterImage -> Html Msg
 charImage image =
@@ -119,13 +155,35 @@ charImage image =
           Nothing -> 
             div[][]
           Just image -> 
+      let
+        fileinfo = (image.maker ++ ":" ++ image.filename)
+      in
          div[class "card-image"][
            div[class "pc-image"][
-              img[src image.src][]
+              img[src image.src, alt fileinfo][],
+              a[class "card-title black-text", href image.makerUrl][
+                text fileinfo
+              ] 
            ]
          ]
 
+charParameters : CharacterParameters -> Html Msg
+charParameters param = 
+          div[class "row"][
+                charaParameter "favorite 【肉体】" param.str
+              , charaParameter "directions_run 【敏捷】" param.dex
+              , charaParameter "visibility 【知覚】" param.sense
+              , charaParameter "search 【精神】" param.mind
+              , charaParameter "toys 【幸運】" param.luck
+              , charaParameter ("cached 【" ++ param.freeName ++ "】") param.free
+          ]
 
+charaParameter : String -> Int -> Html Msg
+charaParameter param value = 
+              div[class "col l6 s12"][
+                i[class "material-icons"][text param],
+                span[class "pc-param-number"][text (toString value)]
+              ]
 -- 更新
 
 
