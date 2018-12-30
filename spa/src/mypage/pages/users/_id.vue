7e8ed77af32dd46ea8d82fcd759eb3bcbe230d4b
hibohiboo <swdice66@gmail.com>
2018-12-30 21:20:31 +0900

    コンテンツのだし分け

--- spa/src/mypage/pages/users/_id.vue
+++ spa/src/mypage/pages/users/_id.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <p>
-      /users/_id.vue
+      UserId: {{userId}}
     </p>
   </div>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        userId: this.$route.params.id
+      }
+    }
+  }
+</script>
