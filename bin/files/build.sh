8bdbcaeea21871d3bbfc23e3a37a02bce8bf0bb0
hibohiboo <swdice66@gmail.com>
2018-12-30 18:24:58 +0900

    ファイルをコピー

---
+++ bin/files/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# このシェルスクリプトのディレクトリの絶対パスを取得。
+bin_dir=$(cd $(dirname $0) && pwd)
+name=${1:-files}
+dir_docker="$bin_dir/../../docker"
+# 拡張子指定に半角空白を入れないように注意。 【{png,jpg}:OK】【{png, jpg}:NG】
+cd $dir_docker && docker-compose run  $name yarn run cpx "/app/src/**/*.{png,jpg}" /app/dist 
