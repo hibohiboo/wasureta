441977a64235579425d33d3f2f55c6498c503ecd
hibohiboo <swdice66@gmail.com>
2018-12-31 09:24:16 +0900

    一旦以前の環境は削除

--- spa/src/mypage/plugins/axios.js
+++
@@ -1,9 +0,0 @@
-export default function ({ $axios }) {
-  $axios.onRequest((config) => {
-    if (process.env.QIITA_TOKEN) {
-      // 実際の開発の場合は store.state.token などを参照することになるはずです
-      config.headers.common['Authorization'] = `Bearer ${process.env.QIITA_TOKEN}`
-    }
-    return config
-  })
-}
