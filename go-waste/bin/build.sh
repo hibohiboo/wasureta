8b1afbc1eea490992494a4ab74d3b42d785add50
hibohiboo <swdice66@gmail.com>
2019-01-03 18:23:43 +0900

    グリッド作成

--- go-waste/bin/build.sh
+++ go-waste/bin/build.sh
@@ -2,11 +2,21 @@
 
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
+parent_dir=$bin_dir/..
+# rm -rf $bin_dir/../app/public/insane
+# rm -rf $bin_dir/../dist/assets
+# rm -rf $bin_dir/../dist/html
+# rm -rf $bin_dir/../pre-dist/assets
 
-rm -rf $bin_dir/../app/public/insane
-rm -rf $bin_dir/../dist/assets
-rm -rf $bin_dir/../dist/html
-rm -rf $bin_dir/../pre-dist/assets
+# fix と postcssは速度が遅いので、別で実行するようにする
+# lint-fix
+# bash $bin_dir/scss/fix.sh 
 
 # html作成
 bash $bin_dir/pug/build.sh
+
+# css作成
+bash $bin_dir/scss/build.sh
+# bash $bin_dir/postcss/build.sh
+
+cp -rf $parent_dir/pre-dist/assets $parent_dir/dist/html/assets 
---
+++ go-waste/bin/postcss/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# このシェルスクリプトのディレクトリの絶対パスを取得。
+bin_dir=$(cd $(dirname $0) && pwd)
+name=${1:-postcss}
+dir_docker="$bin_dir/../../docker"
+
+# docker-composeの起動。 
+cd $dir_docker  && docker-compose run $name yarn run postcss /app/src/assets/css/*.css --no-map --dir /app/dist/assets/css/
