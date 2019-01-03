07fa06c17d2d451c1787adcf4546ac867c1580f5
hibohiboo <swdice66@gmail.com>
2019-01-03 16:24:06 +0900

    使用しないディレクトリを削除

--- nuxt/bin/container_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# このシェルスクリプトのディレクトリの絶対パスを取得。
-dir_bin=$(cd $(dirname $0) && pwd)
-
-cd $dir_bin/../docker && docker-compose build
--- nuxt/src/mypage/app/plugins/moment.js
+++
@@ -1,5 +0,0 @@
-import 'moment/locale/ja'
-import moment from 'moment'
-// 初回時にロケールの背帝が必要となる。全体では一度読み込んだものを使いまわす。
-moment.locale('ja')
-export default moment
