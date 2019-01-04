0dae0b6ddbaf3cbe1d5d7a63af1779e0386d7fb3
hibohiboo <swdice66@gmail.com>
2019-01-05 00:36:05 +0900

    fileのコピー

--- insane/bin/separate/js/build.sh
+++ insane/bin/separate/js/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
 parent_dir=$bin_dir/../../..
 docker_dir=$parent_dir/docker
 dir_docker=$docker_dir
 name=${1:-elm}
 
-# docker-composeの起動。 
 cd $dir_docker  && docker-compose run $name yarn babel /app/separate/pre-pre-dist/ --out-dir /app/separate/pre-dist/
