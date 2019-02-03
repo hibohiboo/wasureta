731dab8a3508cb0afef45bfb43a174c05178ec86
hibohiboo <swdice66@gmail.com>
2019-02-03 12:31:14 +0900

    不要ファイル削除

--- src/assets/js/models/User.ts
+++
@@ -1,9 +0,0 @@
-export default class User {
-  public uid: string;
-  public displayName: string;
-  constructor(user: any) {
-    const { uid, displayName } = user;
-    this.uid = uid;
-    this.displayName = displayName;
-  }
-}
